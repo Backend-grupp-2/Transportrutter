3bd79f646633ca1624a8e6cd158b2720f4ce6af5
Uneditedmold03 <philipwennerholm51@gmail.com>
2024-03-05 20:40:17 +0100

    added url to database

--- src/main/java/com/example/Transportrutter/TransportrutterApplication.java
+++ src/main/java/com/example/Transportrutter/TransportrutterApplication.java
@@ -3,11 +3,15 @@ package com.example.Transportrutter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @SpringBootApplication
 @RestController
+
+
+@RequestMapping("/api/v1/transportrutter")
 public class TransportrutterApplication {
 
 	@GetMapping
